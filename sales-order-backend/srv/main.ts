6271a125701e713ce1ef2caef722605b71065d86
Jose Martins <jfelipem115@gmail.com>
2025-11-10 12:18:40 -0300

    chore: ajusting tscongig.ts and test on main

--- sales-order-backend/srv/main.ts
+++ sales-order-backend/srv/main.ts
@@ -1,2 +1,13 @@
+import { Customer, Customers } from '@models/sales'
+
+const customer: Customer = {
+    email: 'jfelipem115@gmail.com',
+    firstName: 'Jose',
+    lastName: 'Martins',
+    id: '1234'
+};
+
+const customers: Customers = [customer]
+
 const funcao = (variavel: string) => console.log(variavel);
 funcao('Teste do TypeScript');
