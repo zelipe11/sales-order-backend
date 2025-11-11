cf77c6f36641ef947b9c1e83cb88a51525ebabdd
Jose Martins <jfelipem115@gmail.com>
2025-11-11 09:59:21 -0300

    feat: valid register before create SalesOrderHeaders

--- sales-order-backend/srv/main.ts
+++ sales-order-backend/srv/main.ts
@@ -1,4 +1,4 @@
-import { ResultsHandler, Service } from '@sap/cds';
+import cds, { Request, Service } from '@sap/cds';
 import { Customers } from '@models/sales';
 
 export default (service: Service) => {
@@ -8,5 +8,19 @@ export default (service: Service) => {
                 customer.email = `${customer.email}@gmail.com`
             }
         })
-    })
+    });
+    service.before('CREATE', 'SaleOrderHeaders', async (request: Request) => {
+        const params = request.data;
+        if (!params.customer_id) {
+            return request.reject(400, 'Customer invalido')
+        }
+        if (!params.items || params.items?.length === 0) {
+            return request.reject(400, 'Itens invalidos');
+        }
+        const customerQuery = SELECT.one.from('sales.Customers').where({ id: params.customer_id });
+        const customer = await cds.run(customerQuery);
+        if (!customer) {
+            return request.reject(404, 'Customer não encontrado')
+        }
+    });
 }
