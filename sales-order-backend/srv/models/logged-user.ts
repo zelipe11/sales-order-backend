c5b8b5c3a5648624d788737ba9174529afb6b877
Jose Martins <jfelipem115@gmail.com>
2025-11-24 18:04:19 -0300

    feat: add LoggedUserModel

---
+++ sales-order-backend/srv/models/logged-user.ts
@@ -0,0 +1,35 @@
+type LoggedUserProps = {
+    id: string;
+    roles: string[];
+    attributes: LoggedUserAttributesProps;
+};
+
+type LoggedUserAttributesProps = {
+    id: number;
+    groups: string[];
+
+};
+
+export class LoggedUserModel {
+    constructor(private props: LoggedUserProps) {}
+
+    public static create(props: LoggedUserProps) {
+        return new LoggedUserModel(props);
+    }
+
+    public get id() {
+        return this.props.id;
+    }
+
+    public get roles() {
+        return this.props.roles;
+    }
+
+    public get attributes() {
+        return this.props.attributes;
+    }
+
+    public toStringfiedObject(): string {
+        return JSON.stringify(this.props);
+    }
+}
